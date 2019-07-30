50c75d05e89399d954f617b108515ca81069d990
Aaron <aaron_hansen@yahoo.com>
2019-07-30 09:46:32 -0700

    - Moved show tables functionality into the core library.

--- src/main/java/org/iot/dsa/dslink/jdbc/h2/MainNode.java
+++ src/main/java/org/iot/dsa/dslink/jdbc/h2/MainNode.java
@@ -1,7 +1,6 @@
 package org.iot.dsa.dslink.jdbc.h2;
 
 import org.iot.dsa.dslink.jdbc.AbstractMainNode;
-import org.iot.dsa.dslink.jdbc.JDBCv2Helpers;
 import org.iot.dsa.node.DSElement;
 import org.iot.dsa.node.DSInfo;
 import org.iot.dsa.node.DSMap;
@@ -12,34 +11,31 @@ import org.iot.dsa.node.action.ActionResult;
 import org.iot.dsa.node.action.DSAction;
 
 public class MainNode extends AbstractMainNode {
-	
-	@Override
-	protected DSAction makeAddDatabaseAction() {
-		DSAction act = new DSAction.Parameterless() {
+
+    @Override
+    protected String getHelpUrl() {
+        return "https://github.com/iot-dsa-v2/dslink-java-v2-jdbc-h2";
+    }
+
+    @Override
+    protected DSAction makeNewConnectionAction() {
+        DSAction act = new DSAction.Parameterless() {
             @Override
             public ActionResult invoke(DSInfo target, ActionInvocation invocation) {
                 return ((MainNode) target.get()).createNewDatabase(invocation.getParameters());
             }
         };
-        act.addParameter(JDBCv2Helpers.DB_NAME, DSValueType.STRING, null);
-        act.addParameter(JDBCv2Helpers.DB_USER, DSValueType.STRING, null);
-        act.addParameter(JDBCv2Helpers.DB_PASSWORD, DSValueType.STRING, null).setEditor("password");
-        //TODO: add default timeout/poolable options
-        //action.addParameter(new Parameter(JdbcConstants.DEFAULT_TIMEOUT, ValueType.NUMBER));
-        //action.addParameter(new Parameter(JdbcConstants.POOLABLE, ValueType.BOOL, new Value(true)));
+        act.addParameter(DB_NAME, DSValueType.STRING, null);
+        act.addParameter(DB_USER, DSValueType.STRING, null);
+        act.addParameter(DB_PASSWORD, DSValueType.STRING, null).setEditor("password");
         return act;
-	}
-	
-	@Override
-	protected String getHelpUrl() {
-		return "https://github.com/iot-dsa-v2/dslink-java-v2-jdbc-h2";
-	}
-	
-	private ActionResult createNewDatabase(DSMap parameters) {
-        parameters.put(JDBCv2Helpers.DRIVER, DSElement.make("org.h2.Driver"));
-        parameters.put(JDBCv2Helpers.DB_URL, DSElement.make("Not Started"));
+    }
+
+    private ActionResult createNewDatabase(DSMap parameters) {
+        parameters.put(DRIVER, DSElement.make("org.h2.Driver"));
+        parameters.put(DB_URL, DSElement.make("Not Started"));
         DSNode nextDB = new ManagedH2DBConnectionNode(parameters);
-        add(parameters.getString(JDBCv2Helpers.DB_NAME), nextDB);
+        add(parameters.getString(DB_NAME), nextDB);
         return null;
     }
 
